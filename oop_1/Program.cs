ec75ced50fd1a0812076368ac49b609ac9757c06
NadaFayez13 <naditafayez13@gmail.com>
2026-09-15 16:58:12 +0300

    sheet5_Question4

--- oop_1/Program.cs
+++ oop_1/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace oop_1
+﻿using System.Drawing;
+
+namespace oop_1
 {
+    #region question 4
+    /*4-Declare an enum Genre { Fiction, NonFiction, Science }.
+    Add a Genre property to Book, assign it Genre.Science, and print it.*/
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
+    #endregion
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Book book = new Book();
+            book.Title = "Science book";
+            book.Genre = Genre.Science;
+            Console.WriteLine($"Title: {book.Title}");
+            Console.WriteLine($"Genre: {book.Genre}");
+
         }
     }
 }
